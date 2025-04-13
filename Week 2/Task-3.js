b711f6ec7a4f6b6bb547a0db1c4c433466291be6
Pari <113831759+agl724@users.noreply.github.com>
2025-04-13 19:53:23 +0530

    Create Task-3.js

---
+++ Week 2/Task-3.js
@@ -0,0 +1,11 @@
+let n = 7; // you can change this number to test others
+
+let result;
+
+if (n % 2 === 0) {
+  result = "Even";
+} else {
+  result = "Odd";
+}
+
+console.log("The number is:", result); // Output: "The number is: Odd"
