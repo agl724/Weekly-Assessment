d5c79a95c6ee68bc894a5bc21b2026a9e513a247
Pari <113831759+agl724@users.noreply.github.com>
2025-04-13 19:49:46 +0530

    Create Task-1.js

---
+++ Week 2/Task-1.js
@@ -0,0 +1,8 @@
+// Declare a variable age and assign it a value
+let age = 25;
+
+// Declare another variable isAdult based on the condition
+let isAdult = age >= 18;
+
+// Output the result
+console.log("Is Adult:", isAdult); // This will print: Is Adult: true
