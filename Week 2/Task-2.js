c83569c4270c6aef7e75e6bd357a0bd62da775c6
Pari <113831759+agl724@users.noreply.github.com>
2025-04-13 19:50:44 +0530

    Create Task-2.js

---
+++ Week 2/Task-2.js
@@ -0,0 +1,12 @@
+let x = 10;
+let y = 5;
+
+// Perform operations
+let addition = x + y;         // 15
+let multiplication = x * y;   // 50
+let modulus = x % y;          // 0
+
+// Output the results
+console.log("Addition:", addition);
+console.log("Multiplication:", multiplication);
+console.log("Modulus:", modulus);
